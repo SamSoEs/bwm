b2752414a6ee158ab39c1af250eab98f0c51c854
Saman <samaneghbali1@gmail.com>
2022-10-17 11:14:42 -0400

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/shared/Header';
+import RentalHome from './pages/RentalHome'
+
 const App = () => {
   return (
     <div>
       <Header />
+      <RentalHome />
     </div>
   )
     
